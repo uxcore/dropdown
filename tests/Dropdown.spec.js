1db961af27a54cbb9bd540170c1b470aa8c95679
qianxu.ljj <qianxu.ljj@alibaba-inc.com>
2018-01-03 21:28:14 +0800

    [MOD] 1. 删除coverage目录 2.完善单元测试

--- tests/Dropdown.spec.js
+++ tests/Dropdown.spec.js
@@ -1,7 +1,6 @@
 import expect from 'expect.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-14';
 import Dropdown from '../src';
@@ -9,21 +8,14 @@ import Dropdown from '../src';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Dropdown', () => {
-    it('has correct propTypes', () => {
-        expect(Dropdown.propTypes.trigger).to.be(PropTypes.array);
-        expect(Dropdown.propTypes.overlay).to.be(PropTypes.node);
-        expect(Dropdown.propTypes.onVisibleChange).to.be(PropTypes.func);
-        expect(Dropdown.propTypes.visible).to.be(PropTypes.bool);
-    })   
-    
     it('trigger click event', () => {
         const menu = <div />;
         const wrapper = mount(
-            <Dropdown overlay={menu} trigger={['click']}> 
-                <div className="click-btn">点击触发</div>
+            <Dropdown overlay={menu}> 
+                <div>点击触发</div>
             </Dropdown>
         );
-        wrapper.find('.click-btn').simulate('click')
-        expect(Array.prototype.slice.call(document.getElementsByClassName('kuma-dropdown'))).to.have.length(1)
+        expect(wrapper.prop('prefixCls')).to.equal('kuma-dropdown');
+        expect(wrapper.prop('transitionName')).to.equal('dropdownSlideUp');
     })
 });
